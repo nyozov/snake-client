4b54302de41101d3a98f3075571bb771e0085e11
Nick Yozov <n_yozov@hotmail.com>
2021-09-22 17:49:22 +0000

    added ctrl c exit

--- play.js
+++ play.js
@@ -1,11 +1,33 @@
 const net = require("net");
+
 const connect = require('./client')
+
+
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+const handleUserInput = function () {
+  if (data === '\u0003') {
+    process.exit();
+    
+  }
+};
+setupInput()
 
 
 
 
 console.log("Connecting ...");
 connect();
+
+
 
 
 
